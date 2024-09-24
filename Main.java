68839053979ec72948243f6cda56d764f93109ab
August Oo <augustoo5333114@gmail.com>
2024-09-24 11:45:59 +1000

    first commit

--- Main.java
+++
@@ -1,9 +0,0 @@
-package readingroom_2;
-
-
-public class Main {
-    public static void main(String[] args) {
-        // Launch the LoginInterface class
-        LoginInterface.launch(LoginInterface.class, args);
-    }
-}
